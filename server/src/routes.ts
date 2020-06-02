b447a313e952b3e5945c8f34db456bbdcace9ce8
Ailton Eric <aeric@vocacao.org.br>
2020-06-02 18:37:29 -0300

    registration of collection points without image

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,55 @@ routes.get('/items', async (request, response) => {
 
   const serializedItems = items.map(item => {
     return {
+      "id": item.id,
       "title": item.title,
       "image-url": `http://localhost:3333/resources/${item.image}`,
     };
   });
 
   return response.json(serializedItems);
-})
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    image,
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedId = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const point_id = insertedId[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    };
+  });
+
+  await trx('point_items').insert(pointItems);
+
+  trx.commit();
+
+  return response.json({ success: true });
+});
 
 export default routes;
