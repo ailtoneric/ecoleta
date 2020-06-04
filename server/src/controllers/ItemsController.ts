41d1c60fc28f68968472427a79f2a500cf83ee9c
Ailton Eric <aeric@vocacao.org.br>
2020-06-04 15:51:45 -0300

    correcting item return name

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       return {
         "id": item.id,
         "title": item.title,
-        "image-url": `http://localhost:3333/resources/${item.image}`,
+        "image_url": `http://localhost:3333/resources/${item.image}`,
       };
     });
   
