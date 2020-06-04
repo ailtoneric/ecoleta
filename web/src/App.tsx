5462fec973ca3425a93a6e10b407d61021c8f660
Ailton Oliveira <ailtonoliveira@mac.local>
2020-06-04 00:45:43 -0300

    create home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [ counter, setCounter ] = useState(0);
-  
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title={`Contador: ${counter}`} />
-      <h1>Hello World!</h1>
-
-      <button onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Home />
   );
 }
 
